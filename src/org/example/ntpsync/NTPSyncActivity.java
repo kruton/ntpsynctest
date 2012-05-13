fd9cc36a6bfafaea19232b7f6e9370f5c728998c
Kenny Root <kenny@the-b.org>
2012-05-12 17:29:53 -0700

    Add in crude NTP syncing

---
+++ src/org/example/ntpsync/NTPSyncActivity.java
@@ -0,0 +1,103 @@
+package org.example.ntpsync;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.widget.TextView;
+
+public class NTPSyncActivity extends Activity {
+	private static final String TAG = "NTPSyncActivity";
+
+	// private NtpServer mServer;
+
+	private NTPSyncService mBoundService;
+
+	private TextView mTimeDisplay;
+
+	private DateFormat mFormat;
+
+	private Calendar mCalendar = Calendar.getInstance();
+
+	private Handler mTimeHandler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			final NTPSyncService service = mBoundService;
+			if (service == null) {
+				return;
+			}
+
+			final long realTime = System.currentTimeMillis() + mBoundService.getOffset();
+			mCalendar.setTimeInMillis(realTime);
+			mTimeDisplay.setText(mFormat.format(mCalendar.getTime()));
+			mTimeDisplay.postInvalidate();
+
+			final long delayMs = (1000 - (realTime % 1000)) + 50;
+			sendEmptyMessageDelayed(0, delayMs);
+		}
+	};
+
+	private ServiceConnection mConnection = new ServiceConnection() {
+		public void onServiceConnected(ComponentName className, IBinder service) {
+			mBoundService = ((NTPSyncService.LocalBinder) service).getService();
+			mTimeHandler.sendEmptyMessage(0);
+		}
+
+		public void onServiceDisconnected(ComponentName className) {
+			mBoundService = null;
+		}
+	};
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		mTimeDisplay = (TextView) findViewById(R.id.time1);
+
+		mFormat = SimpleDateFormat.getTimeInstance(DateFormat.FULL);
+		// Button b = (Button) findViewById(R.id.button1);
+		// b.setOnClickListener(new OnClickListener() {
+		// @Override
+		// public void onClick(View v) {
+		// try {
+		// mServer.start();
+		// } catch (IOException e) {
+		// Log.e(TAG, "Can't start server", e);
+		// }
+		// }
+		// });
+		// mServer = new MyNtpServer();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+
+		// mServer.stop();
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+
+		Intent i = new Intent(this, NTPSyncService.class);
+		bindService(i, mConnection, BIND_AUTO_CREATE);
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+
+		unbindService(mConnection);
+	}
+}
---
+++ src/org/example/ntpsync/NTPSyncService.java
@@ -0,0 +1,132 @@
+/**
+ * 
+ */
+package org.example.ntpsync;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.apache.commons.net.ntp.NTPUDPClient;
+import org.apache.commons.net.ntp.TimeInfo;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+
+/**
+ * @author kenny
+ * 
+ */
+public class NTPSyncService extends Service {
+	private static final String TAG = "NtpSyncService";
+
+	private static final String DEFAULT_NTP_POOL_HOST = "pool.ntp.org";
+
+	private HandlerThread mThread;
+
+	private NtpHandler mHandler;
+
+	private final IBinder mBinder = new LocalBinder();
+
+	public class LocalBinder extends Binder {
+		NTPSyncService getService() {
+			return NTPSyncService.this;
+		}
+	}
+
+	private static class NtpHandler extends Handler {
+		private static final int INITIAL_SETUP = 1;
+
+		private static final int SYNC_TIME = 2;
+
+		private boolean mIsInitialized;
+
+		private InetAddress mHost;
+
+		private NTPUDPClient mClient;
+
+		private long mOffset;
+
+		public NtpHandler(Looper looper) {
+			super(looper);
+		}
+
+		@Override
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case INITIAL_SETUP:
+				if (!mIsInitialized) {
+					mClient = new NTPUDPClient();
+					mClient.setDefaultTimeout(500000);
+
+					try {
+						mHost = InetAddress.getByName(DEFAULT_NTP_POOL_HOST);
+						mIsInitialized = true;
+						sendEmptyMessage(SYNC_TIME);
+						Log.i(TAG, "Looked up NTP host " + DEFAULT_NTP_POOL_HOST + " at address "
+								+ mHost.getHostAddress());
+					} catch (UnknownHostException e) {
+						Log.w(TAG, "Couldn't lookup NTP host " + DEFAULT_NTP_POOL_HOST
+								+ "; will try again");
+						sendEmptyMessageDelayed(INITIAL_SETUP, 10 * 1000);
+					}
+				}
+				break;
+			case SYNC_TIME:
+				try {
+					TimeInfo timeInfo = mClient.getTime(mHost, NTPUDPClient.DEFAULT_PORT);
+					timeInfo.computeDetails();
+					mOffset = timeInfo.getOffset();
+				} catch (IOException e) {
+					Log.w(TAG, "Could not get time info", e);
+				}
+
+				sendEmptyMessageDelayed(SYNC_TIME, 60 * 1000);
+				break;
+			}
+		}
+
+		public void startSync() {
+			sendEmptyMessage(INITIAL_SETUP);
+		}
+
+		public long getOffset() {
+			return mOffset;
+		}
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return mBinder;
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		mThread = new HandlerThread(getClass().getSimpleName());
+		mThread.start();
+
+		mHandler = new NtpHandler(mThread.getLooper());
+
+		mHandler.startSync();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+
+		mThread.quit();
+	}
+
+	public long getOffset() {
+		return mHandler.getOffset();
+	}
+}
